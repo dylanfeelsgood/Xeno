b55ba4d205b8897ec9f1eb72ddf635e602142c27
dylanfeelsgood <dylanfeelsgood@gmail.com>
2026-06-08 12:44:42 +0800

    day02

--- day02/05猜随机数.py
+++ day02/05猜随机数.py
@@ -4,12 +4,22 @@ from unittest import result
 random_number = random.randint(0,20)
 print("随机数已经生成，请猜猜看")
 
-while True:
- guess_number = int(input("Enter your guess number: "))
- if guess_number < random_number:
-     print("Your guess is too low")
- elif  guess_number > random_number:
-      print("Your guess is too high")
- else:
-      print("You guessed the number")
-      break
+# while True:
+#  guess_number = int(input("Enter your guess number: "))
+#  if guess_number < random_number:
+#      print("Your guess is too low")
+#  elif  guess_number > random_number:
+#       print("Your guess is too high")
+#  else:
+#       print("You guessed the number")
+#       break
+
+guess_number = None
+while guess_number != random_number:
+    guess_number = int(input("请输入猜的数字"))
+    if guess_number > random_number:
+        print("唔，猜大了呢")
+    elif guess_number < random_number:
+        print("这次猜小了")
+    else:
+        print("猜对了，恭喜恭喜")
